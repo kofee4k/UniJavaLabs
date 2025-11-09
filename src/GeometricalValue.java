c036e5c5be8b4674367691011d977e7a3809ba8f
kofee4k <109113761+kofee4k@users.noreply.github.com>
2025-11-09 18:51:22 +0300

    Add files via upload

---
+++ src/GeometricalValue.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GeometricalValue {
+    static double area(int a, int b, int c, int d, int e, int f) {
+        double s = 0.5 * Math.abs(a * (d - f) + c * (f - b) + e * (b - d));
+        return s;
+    }
+    public static void  main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = 100;
+        int a = sc.nextInt()*N;
+        int b = sc.nextInt()*N;
+        int c = sc.nextInt()*N;
+        int d = sc.nextInt()*N;
+        int e = sc.nextInt()*N;
+        int f = sc.nextInt()*N;
+
+        double result = area(a, b, c, d, e, f);
+
+        System.out.printf("%.2f%n", result);
+
+    }
+}
